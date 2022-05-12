7535937f99d3301b2d22f50149a60516573b80ff
unknown <ni95@purdue.edu>
2022-05-12 18:08:42 -0500

    Retrieves floor price of parsed collections, but doesn't display

--- src/components/RetrieveCollections.js
+++ src/components/RetrieveCollections.js
@@ -1,19 +1,30 @@
 import React from "react";
 import axios from "axios";
 
-export default class RetrieveCollections extends React.Component {
+export default class RetrieveCollections extends React.Component { 
     state = {
         collections: [],
+        search_address: "0xF6429FaFDb6166D145f30372717a23B966373032",
+        floor: 0,
       }
 
     componentDidMount() {
-        axios.get('https://api.opensea.io/api/v1/collections?asset_owner=0xF6429FaFDb6166D145f30372717a23B966373032&offset=0&limit=300')
+        axios.get('https://api.opensea.io/api/v1/collections?asset_owner=' + this.state.search_address + '&offset=0&limit=300')
           .then(res => {
             this.setState({ 
                 collections: res.data,
             });
           })
       }
+
+    getCollectionFloor(slug){
+        axios.get('https://api.opensea.io/api/v1/collection/' + slug + '/stats')
+        .then(res => {
+            console.log(res.data.stats.floor_price)
+            return res.data.stats.floor_price
+        })
+    }
+
     render() {
       console.log(this.state.collections)
       return (
@@ -22,7 +33,7 @@ export default class RetrieveCollections extends React.Component {
             {
                 this.state.collections
                     .map(collection =>
-                    <li key={collection.id}>{collection.name} - {collection.owned_asset_count}</li>
+                    <li key={collection.id}>{collection.name} - {collection.owned_asset_count} - {this.getCollectionFloor(collection.slug)}</li>
                     )
             }
             </ul>

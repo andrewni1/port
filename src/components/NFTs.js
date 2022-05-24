341331d5f2ef57c21e094766740f9b4c8d42d9a0
unknown <ni95@purdue.edu>
2022-05-24 15:27:08 -0500

    Temporary address search box

--- src/components/NFTs.js
+++ src/components/NFTs.js
@@ -12,56 +12,73 @@ export default class NFTs extends React.Component {
       assets: [],
       collections: [],
       stats: [],
+      floor_prices: [],
       search_address: "0xF6429FaFDb6166D145f30372717a23B966373032",
     }
 
     componentDidMount() {
-        axios.get('https://api.opensea.io/api/v1/assets?owner=' + this.state.search_address + '&order_direction=desc&limit=200&include_orders=false', this.state.options)
-        .then(res => {
-            this.setState({ 
-              assets: res.data.assets,
-            });
-          })
-        axios.get('https://api.opensea.io/api/v1/collections?asset_owner=' + this.state.search_address + '&offset=0&limit=300')
-        .then(res => {
+      axios.get('https://api.opensea.io/api/v1/assets?owner=' + this.state.search_address + '&order_direction=desc&limit=200&include_orders=false', this.state.options)
+      .then(res => {
           this.setState({ 
-              collections: res.data,
-              stats: res.data.stats
+            assets: res.data.assets,
           });
         })
-      }
+      axios.get('https://api.opensea.io/api/v1/collections?asset_owner=' + this.state.search_address + '&offset=0&limit=300')
+      .then(res => {
+        this.setState({ 
+            collections: res.data,
+            stats: res.data.stats
+        });
+      })
+    }
 
-      getCollectionFloor(slug){
-        axios.get('https://api.opensea.io/api/v1/collection/' + slug + '/stats')
-        .then(res => {
-            console.log(res.data.stats.floor_price)
-            return res.data.stats.floor_price
-        })
-      }
-    
+    getCollectionFloor(slug){
+      axios.get('https://api.opensea.io/api/v1/collection/' + slug + '/stats')
+      .then(res => {
+          console.log(res.data.stats.floor_price)
+          this.setState({ 
+            floor_prices: [...this.state.floor_prices, res.data.stats.floor_price]
+          });
+          return this.state.floor_prices;
+      })
+    }
+
+    changeAddress = (e) => {
+      this.setState({
+        search_address: e.target.value
+      });
+      console.log(e.target.value)
+    }
     render() {
       // console.log(this.state.assets)
       return (
-        <div className="card-container">
-          {this.state.assets.map(asset => {
-            if (!asset.collection.name.includes('Unidentified contract'))
-            return (
-            <div className="card-contents">
-              <a className="asset-card" href={asset.permalink} target="_blank" rel="noreferrer">
-                <img className="asset_img_container"
-                  src={asset.image_url}
-                  alt="asset_image"
-                />
-                <div>{asset.name}</div>
-                <div>{asset.collection.name}</div>
-                {/* <div className="words">
-                  {this.getCollectionFloor(asset.collection.slug)}
-                </div> */}
-              </a>
-            </div>
-          )
-          }
-          )}
+        <div>
+          <div>
+          <p>
+            Type here to change address.
+            <input type="text" onChange={this.changeAddress}/>
+          </p>
+          </div>
+          <div className="card-container">
+            {this.state.assets.map(asset => {
+              if (!asset.collection.name.includes('Unidentified contract'))
+              return (
+                <div className="card-contents">
+                  <a className="asset-card" href={asset.permalink} target="_blank" rel="noreferrer">
+                    <img className="asset_img_container"
+                      src={asset.image_url}
+                      alt="asset_image"
+                    />
+                    <div>{asset.name}</div>
+                    <div>{asset.collection.name}</div>
+                    {/* <div className="words">
+                      {this.getCollectionFloor(asset.collection.slug)}
+                    </div> */}
+                  </a>
+                </div>
+              )
+            })}
+          </div>
         </div>
       )
     }

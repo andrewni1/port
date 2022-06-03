9c277d42e863c43a6e05782dde1f6d98a5ec483c
unknown <ni95@purdue.edu>
2022-06-03 07:29:44 -0500

    More filters for rendering prices and placeholder images

--- src/components/Eth.js
+++ src/components/Eth.js
@@ -1,22 +1,22 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { ethers } from "ethers";
 import axios from 'axios';
 import { BsFillBriefcaseFill, BsFillCollectionFill } from "react-icons/bs";
 import { IoIosPhotos } from "react-icons/io";
 import { FaCoins, FaEthereum } from "react-icons/fa";
+import nftPlaceholder from './nft-placeholder.jpg'
 import './Eth.css'
 
 function Eth() {
     const [assets, setAssets] = useState([]);
     const [collections, setCollections] = useState([]);
     const [ethPrice, setEthPrice] = useState();
-    const [nftFilter, setNftFilter] = useState('collections')
+    const [nftFilter, setNftFilter] = useState('assets')
     // const [totalEthNft, setTotalEthNft] = useState(0);
     const [walletAddress, setWalletAddress] = useState(null);
-
-    const [data, setdata] = useState({
+    const [data, setData] = useState({
         address: "",
-        balance: null,
+        balance: null
     });
 
     const btnhandler = () => {
@@ -29,14 +29,14 @@ function Eth() {
         }
     };
 
-    const getbalance = (address) => {
+    const getBalance = (address) => {
         window.ethereum
             .request({ 
                 method: "eth_getBalance", 
                 params: [address, "latest"] 
             })
             .then((balance) => {
-                setdata({
+                setData({
                     balance: ethers.utils.formatEther(balance),
                 });
             });
@@ -44,24 +44,14 @@ function Eth() {
     
     const accountChangeHandler = (account) => {
         setWalletAddress(account);
-        setdata({
-            address: account,
-        });
-        
-        getbalance(account);
-    };
+        setData({ address: account });
+        getBalance(account);
 
-    const options = {
-        method: 'GET',
-        headers: {Accept: 'application/json', 'X-API-KEY': 'd164f34100f24f3bab3cc79302606609'}
-    };
-      
-    useEffect(() => {
-        axios.get('https://api.opensea.io/api/v1/assets?owner=' + walletAddress + '&order_direction=desc&limit=200&include_orders=false', options)
+        axios.get('https://api.opensea.io/api/v1/assets?owner=' + account + '&order_direction=desc&limit=200&include_orders=false', options)
         .then(res => {
             setAssets(res.data.assets);
         })
-        axios.get('https://api.opensea.io/api/v1/collections?asset_owner=' + walletAddress + '&offset=0&limit=300', options)
+        axios.get('https://api.opensea.io/api/v1/collections?asset_owner=' + account + '&offset=0&limit=300', options)
         .then(res => {
             setCollections(res.data);
         })
@@ -69,7 +59,12 @@ function Eth() {
         .then(res => {
             setEthPrice(res.data.ethereum.usd)
         })
-    })
+    };
+
+    const options = {
+        method: 'GET',
+        headers: {Accept: 'application/json', 'X-API-KEY': 'd164f34100f24f3bab3cc79302606609'}
+    };
 
     const nftFilterAssets = () => {
         setNftFilter('assets')
@@ -79,14 +74,28 @@ function Eth() {
         setNftFilter('collections')
     }
 
+    const inputHandler = (e) => {
+        setWalletAddress(e.target.value)
+        console.log(e.target.value)
+    }
+
     if (data.address === "") {
         return (
             <div>
+                <img src={nftPlaceholder} alt='nothing'/>
                 <button onClick={btnhandler}>
                     Connect to wallet
                 </button>
+                <div>
+                    <input
+                        type="text"
+                        onChange={inputHandler}
+                        placeholder="Wallet Address"
+                        value={walletAddress}
+                    />
+                    {/* <button onClick={accountChangeHandler(walletAddress)}>Search</button> */}
+                </div>
             </div>
-            
         )
     } else return (
         <div>
@@ -97,11 +106,13 @@ function Eth() {
                 </div>
                 <div className="nft-stats-container-1">
                     <div className='stat-box'><BsFillBriefcaseFill className='stat-image'/> VALUE: ${Math.round(data.balance * ethPrice * 100) / 100}</div>
-                    <div className='stat-box'><FaCoins className='stat-image'/> TOKENS: XXX</div>
-                    <div className='stat-box'><FaCoins className='stat-image'/> DOMINANT TOKEN: XXX </div>
+                    <div className='stat-box'><FaCoins className='stat-image'/> TOKENS: 1</div>
+                    <div className='stat-box'><FaCoins className='stat-image'/> DOMINANT TOKEN: Ethereum </div>
                 </div>
-                <div>
-                    Eth {data.balance}
+                <div className='token-list'>
+                    <div className='token-container'>
+                        <FaEthereum /> | Ethereum {Math.round(data.balance * 100) / 100}
+                    </div>
                 </div>
             </div>
 
@@ -132,18 +143,35 @@ function Eth() {
                         return (
                             <div className='card-contents'>
                                 <a className="asset-card" href={'https://opensea.io/collection/' + collection.slug} target="_blank" rel="noreferrer">
-                                    <img className="asset_img_container"
-                                        src={collection.image_url}
-                                        alt="collection_image"
-                                    />
+                                    {collection.image_url !== null
+                                        ? <img className="asset_img_container"
+                                            src={collection.image_url}
+                                            alt='collection'
+                                        />                                            
+                                        : <img className="asset_img_container"
+                                            src={nftPlaceholder}
+                                            alt='collection'
+                                        />
+                                    }
                                     <div className='card-info'>
                                         {collection.name}
                                         <div>Quantity: {collection.owned_asset_count}</div>
-                                        <div className='eth-val'>Avg. Price:<FaEthereum />{Math.round(((collection.stats.one_day_average_price + collection.stats.seven_day_average_price) / 2) * 100) / 100}</div>
-                                        <div className='card-value'>
-                                            <div className='eth-val'><FaEthereum /> {Math.round(((collection.stats.one_day_average_price + collection.stats.seven_day_average_price) / 2) * collection.owned_asset_count * 100) / 100}</div>
-                                            <div className='dollar-val'>${Math.round(((collection.stats.one_day_average_price + collection.stats.seven_day_average_price) / 2) * collection.owned_asset_count * ethPrice * 100) / 100}</div>
-                                        </div>
+                                        {(collection.stats.one_day_average_price !== 0 && collection.stats.seven_day_average_price !== 0)
+                                            ? <div>
+                                                <div className='eth-val'>Avg. Price:<FaEthereum />{Math.round(((collection.stats.one_day_average_price + collection.stats.seven_day_average_price) / 2) * 100) / 100}</div>
+                                                <div className='card-value'>
+                                                    <div className='eth-val'><FaEthereum /> {Math.round(((collection.stats.one_day_average_price + collection.stats.seven_day_average_price) * collection.owned_asset_count / 2) * 100) / 100}</div>
+                                                    <div className='dollar-val'>${Math.round(((collection.stats.one_day_average_price + collection.stats.seven_day_average_price) / 2) * collection.owned_asset_count * ethPrice * 100) / 100}</div>
+                                                </div>
+                                            </div>
+                                            : <div>
+                                                <div>Avg. Price:<FaEthereum />{Math.round(collection.stats.thirty_day_average_price * 100) / 100}</div>
+                                                <div className='card-value'>
+                                                    <div className='eth-val'><FaEthereum /> {Math.round(collection.stats.thirty_day_average_price * collection.owned_asset_count * 100) / 100}</div>
+                                                    <div className='dollar-val'>${Math.round(collection.stats.thirty_day_average_price * collection.owned_asset_count * ethPrice * 100) / 100}</div>
+                                                </div>
+                                            </div>
+                                        }
                                     </div>
                                 </a>
                             </div>
@@ -155,19 +183,36 @@ function Eth() {
                         return (
                             <div className="card-contents">
                                 <a className="asset-card" href={asset.permalink} target="_blank" rel="noreferrer">
-                                    <img className="asset_img_container"
-                                        src={asset.image_url}
-                                        alt="asset_image"
-                                    />
+                                    {asset.image_url !== null
+                                        ? <img className="asset_img_container"
+                                            src={asset.image_url}
+                                            alt='asset'
+                                        />                                            
+                                        : <img className="asset_img_container"
+                                            src={nftPlaceholder}
+                                            alt='asset'
+                                        />
+                                    }
                                     <div className='card-info'>
-                                        {asset.name}
+                                        {asset.name !== null
+                                            ? <div>{asset.name}</div>
+                                            : <div>{asset.collection.name}</div>
+                                        }
                                         {collections.map(collection => {
                                             if (!collection.name.includes('Unidentified contract'))
                                             if (collection.slug === asset.collection.slug)
                                             return (
-                                                <div className='card-value'>
-                                                    <div className='eth-val'><FaEthereum /> {Math.round(((collection.stats.one_day_average_price + collection.stats.seven_day_average_price) / 2) * 100) / 100}</div>
-                                                    <div className='dollar-val'>${Math.round(((collection.stats.one_day_average_price + collection.stats.seven_day_average_price) / 2) * ethPrice * 100) / 100}</div>
+                                                <div>
+                                                    {(collection.stats.one_day_average_price !== 0 && collection.stats.seven_day_average_price !== 0)
+                                                        ? <div className='card-value'>
+                                                            <div className='eth-val'><FaEthereum /> {Math.round(((collection.stats.one_day_average_price + collection.stats.seven_day_average_price) / 2) * 100) / 100}</div>
+                                                            <div className='dollar-val'>${Math.round(((collection.stats.one_day_average_price + collection.stats.seven_day_average_price) / 2) * ethPrice * 100) / 100}</div>
+                                                        </div>
+                                                        : <div className='card-value'>
+                                                            <div className='eth-val'><FaEthereum /> {Math.round(collection.stats.thirty_day_average_price * 100) / 100}</div>
+                                                            <div className='dollar-val'>${Math.round(collection.stats.thirty_day_average_price * ethPrice * 100) / 100}</div>
+                                                        </div>
+                                                    }
                                                 </div>
                                             )
                                         })}
